561566a2bb2a839b8a25d71956a06cd650751405
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-05-14 15:19:20 +0300

    Time: 275 ms (47.30%), Space: 14 MB (92.44%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        # get len(m)
+        # loop through len(m)
+        # get sub array
+        # sort
+        # loop to check if arithmetic and store val in bool arr
+        
+        m = len(l)
+        answer = [True]*m
+        
+        for i in range(m):
+            start_index = l[i]
+            end_index = r[i]
+            
+            sub_arr = nums[start_index:end_index+1]
+            
+            sub_arr.sort()
+            
+            diff = sub_arr[1] - sub_arr[0]
+            
+            for j in range(len(sub_arr)-1):
+                if sub_arr[j + 1] -  sub_arr[j] != diff: answer[i] = False
+        
+        return answer
