b87274f100e0884b7e63dda696cea54ef791d50d
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-05-17 09:58:29 +0300

    Time: 54 ms (26.30%), Space: 14 MB (20.45%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,6 +1,8 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        st = []
+        st = []#0,2
+        # example case
+        # s = "(u(love)i)"
         lenn = len(s)
         for i in range(lenn):
 
@@ -13,13 +15,16 @@ class Solution:
             # after the last encountered opening
             # bracket till the current character
             elif (s[i] == ')'):
+                # i = 7
                 temp = s[st[-1]:i + 1]
+                # s[2:8]
+                # (love)
                 s = s[:st[-1]] + temp[::-1] + s[i + 1:]
-                del st[-1]
+                # s = s[:2] + evol + s[8:]
+                st.pop()
 
         # To store the modified string
         res = ""
         for i in range(lenn):
-            if (s[i] != ')' and s[i] != '('):
-                res += (s[i])
+            if s[i] not in ["(",")"]: res += s[i]
         return res
