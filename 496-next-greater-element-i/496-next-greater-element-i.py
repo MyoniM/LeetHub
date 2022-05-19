a42833a99b8fcf01024ef4292e4d5ab3c714ff4c
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-05-19 18:05:41 +0300

    Time: 72 ms (58.12%), Space: 14.2 MB (14.37%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = [-1] * len(nums1)
+        stack = []
+        num1_hash = {e:i for i,e in enumerate(nums1)}
+
+        for e in nums2:
+            while stack and stack[-1] < e:
+                ans[num1_hash[stack.pop()]] = e
+                
+            if e in num1_hash: stack.append(e)
+                
+        return ans
