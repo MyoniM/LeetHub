6ca7fe6217a09ac3c694ffe541c291f549970737
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-05-24 11:19:43 +0300

    Time: 45 ms (75.45%), Space: 14.4 MB (9.79%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -4,16 +4,18 @@ class Solution:
         if n == 1: return 1
         arr = list(range(1,n+1))
         
+        # play takes players arr and starting index
         def play(arr, i):
             l = len(arr)
+            # if only 1 player remains, return
             if l == 1: return arr[0]
-            s = 0
-            c = i
-            while s < k:
-                s+=1
-                c=(c+1)%l
-
-            arr.pop(c-1)
-            return play(arr,(c-1)%l)
+            # step = (start index + k moves) % l
+            # '%' b/c we dont want to go out of the boundary
+            # -1 to get the index to pop
+            step = ((i+k)%l) - 1
+            arr.pop(step)
+            # do the same thing after poping the element at step -1
+            # continue from popped index + 1
+            return play(arr,step%l)
         
         return play(arr,0)
