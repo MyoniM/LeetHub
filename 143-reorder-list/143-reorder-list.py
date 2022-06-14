1debb2fa64348f6016c302a2ab72e1b6d084306b
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-14 19:06:37 +0300

    Time: 96 ms (84.50%), Space: 24 MB (38.96%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -3,34 +3,29 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# 
+
+# O(n) time | O(1) space
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head.next: return head
-        slow = head
-        fast = head.next
+    def reorderList(self, head: ListNode) -> None:
+        # find middle
+        slow, fast = head, head.next
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
-            
-        first = head
-        tempNext = slow.next
-        slow.next = None
-        second = self.reverseLinkedList(tempNext)
+            fast = fast.next.next
         
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        # merge two halfs
+        first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-
-    def reverseLinkedList(self, currentNode):
-        prevNode = None
-        while True:
-            tempNext = currentNode.next
-            currentNode.next = prevNode
-            if tempNext is None: break
-            prevNode = currentNode
-            currentNode = tempNext
-        return currentNode
-        
