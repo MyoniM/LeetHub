f3250f341aac17d32a6bc547d14a47017dfbde05
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-15 10:47:37 +0300

    Time: 597 ms (53.60%), Space: 20.9 MB (72.87%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -6,8 +6,6 @@ class Solution:
         while l <= r:
             if people[l] + people[r] <= limit:
                 l += 1
-                r -= 1
-            else:
-                r -= 1
+            r -= 1
             count += 1
         return count
