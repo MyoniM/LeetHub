948872962e9c8ad896cfb9bb78f3d4acb7dcaed8
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-15 13:52:43 +0300

    Time: 239 ms (80.55%), Space: 25.4 MB (27.35%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,12 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Time intesive sln, O(n^2) time worst case | O(1) space
         k = k % len(nums)
-        # while k:
-        #     k -= 1
-        #     last = nums.pop(-1)
-        #     nums.insert(0, last)
         
         l, r = 0, len(nums) - 1
         while l < r:
