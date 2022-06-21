ca5bf7e913108af1d417b56f482da0bf1c2360ed
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-21 22:32:36 +0300

    Time: 3743 ms (5.00%), Space: 14.8 MB (71.91%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,11 +1,30 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        h = []
-        for n in nums:
-            heapq.heappush(h, -1 * n)
-            
+        # Best and average O(n) time | O(1) space
+        # Worst O(n^2) time | O(1) space
+        position = len(nums) - k
+        return self.quickSelectHelper(nums, 0, len(nums) - 1, position)
+    
+    def quickSelectHelper(self, array, startIdx, endIdx, position):
         while True:
-            n = heapq.heappop(h)
-            k -= 1
-            if k == 0:
-                return n * -1
+            pivot = startIdx
+            left = startIdx + 1
+            right = endIdx
+            
+            while left <= right:
+                if array[left] > array[pivot] and array[right] < array[pivot]:
+                    self.swap(left, right, array)
+                if array[left] <= array[pivot]:
+                    left += 1
+                if array[right] >= array[pivot]:
+                    right -= 1
+            self.swap(right, pivot, array)
+            if right == position:
+                return array[right]
+            elif right < position:
+                startIdx = right + 1
+            else:
+                endIdx = right - 1
+                    
+    def swap(self, i, j, array):
+        array[i], array[j] = array[j], array[i]
