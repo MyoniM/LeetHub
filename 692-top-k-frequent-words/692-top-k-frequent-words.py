16878acc31645c8ee6811c382da72cee2e21aec6
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-26 18:20:49 +0300

    Time: 61 ms (87.82%), Space: 14.1 MB (27.28%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -8,12 +8,12 @@ class Solution:
         
         # Solution 1 (Max Heaps) this approach have O(k*log(n)) time complexity
 
-        maxHeap = [(-freq[k], k) for k in freq.keys()]
-        heapq.heapify(maxHeap)
-        for _ in range(k):
-            __, maxFreqWord = heapq.heappop(maxHeap)
-            result.append(maxFreqWord)
-        return result
+        # maxHeap = [(-freq[k], k) for k in freq.keys()]
+        # heapq.heapify(maxHeap)
+        # for _ in range(k):
+        #     __, maxFreqWord = heapq.heappop(maxHeap)
+        #     result.append(maxFreqWord)
+        # return result
     
         # Solution 2 (Min Heaps) this approach have O(n*log(k)) time complexity
         
@@ -21,26 +21,26 @@ class Solution:
         # we need some way to compare the words if their frequency is equal
         # we cant use tuples for this as we will be needing the larger string first
         # we do that by creating a custom class (Word)
-#         class Word:
-#             def __init__(self, freq, word):
-#                 self.word = word
-#                 self.freq = freq
-#             def __lt__(self, other):
-#                 # if two words have same frequency
-#                 # we need the larger word to be at the top
-#                 if self.freq == other.freq:
-#                     return self.word > other.word 
-#                 return self.freq < other.freq
+        class Word:
+            def __init__(self, freq, word):
+                self.word = word
+                self.freq = freq
+            def __lt__(self, other):
+                # if two words have same frequency
+                # we need the larger word to be at the top
+                if self.freq == other.freq:
+                    return self.word > other.word 
+                return self.freq < other.freq
 
-#         minHeap = []
-#         for key in freq.keys():
-#             heapq.heappush(minHeap, Word(freq[key], key))
-#             if len(minHeap) > k:
-#                 heapq.heappop(minHeap)
-#         while minHeap:
-#             maxFreqWord = heapq.heappop(minHeap)
-#             result.append(maxFreqWord.word)
-#         return reversed(result)
+        minHeap = []
+        for key in freq.keys():
+            heapq.heappush(minHeap, Word(freq[key], key))
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        while minHeap:
+            maxFreqWord = heapq.heappop(minHeap)
+            result.append(maxFreqWord.word)
+        return reversed(result)
         
         # both solutions have O(n) space complexity b/c of the hashmap
         # in worst case, all the elements could have frequency of 1, hence take O(n) space
