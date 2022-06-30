7c9a9728997683d9932b39a448dccfc1f637f4fc
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-30 09:41:52 +0300

    Time: 623 ms (5.02%), Space: 15.3 MB (20.60%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
@@ -4,10 +4,5 @@ class Solution:
         leftMostProd = nums[0] * nums[1]
         rightMostProd = nums[-2] * nums[-3]
         
-        leftIsBig = leftMostProd > rightMostProd
-        endIsPositive = nums[-1] > 0
+        return max(nums[-1] * leftMostProd, nums[-1] * rightMostProd)
         
-        if endIsPositive:
-            return nums[-1] * leftMostProd if leftIsBig else nums[-1] * rightMostProd
-        else:
-            return nums[-1] * rightMostProd if leftIsBig else nums[-1] * leftMostProd
