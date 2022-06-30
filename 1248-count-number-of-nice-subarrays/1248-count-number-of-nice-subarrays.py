46e289db048cb5dd385825717577cabaf81cac1a
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-06-30 11:28:23 +0300

    Time: 1583 ms (16.96%), Space: 21.6 MB (17.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,15 +1,16 @@
+# O(n) time | O(n) space b/c of the hashmap
+
+# you can find another variation of this problem here
+# https://www.youtube.com/watch?v=fFVZt-6sgyo
+
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        for i, n in enumerate(nums):
-            if n % 2 == 0: nums[i] = 0
-            else: nums[i] = 1
-                
         ans = 0
         pSum = 0
         hashMap = defaultdict(int)
         hashMap[0] = 1
         for n in nums:
-            pSum += n
+            pSum += 0 if n % 2 == 0 else 1
             diff = pSum - k
             ans += hashMap.get(diff, 0)
             hashMap[pSum] += 1
