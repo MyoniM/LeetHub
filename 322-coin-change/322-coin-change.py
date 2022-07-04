a80f0fcbef43606e379fcbd99f3182cd3a60eb32
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-04 14:40:10 +0300

    Time: 2554 ms (29.26%), Space: 25.8 MB (18.06%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,11 +1,12 @@
 class Solution:
+#   TOP-DOWN WITH MEMOIZATION
     def coinChange(self, coins: List[int], amount: int) -> int:
         visited = {}
         minUsedCoinsArray = self.getUsedCoins(amount, coins, visited)
-        return len(minUsedCoinsArray) if minUsedCoinsArray is not None else -1
+        return minUsedCoinsArray if minUsedCoinsArray is not None else -1
             
     def getUsedCoins(self, amount, coins, visited):
-        if amount == 0: return []
+        if amount == 0: return 0
         if amount < 0: return None
         if amount in visited: return visited[amount]
         
@@ -14,9 +15,12 @@ class Solution:
             newAmount = amount - coin
             usedCoins = self.getUsedCoins(newAmount, coins, visited)
             if usedCoins is not None:
-                if minCountArray is None or len(usedCoins) + 1 < len(minCountArray):  
-                    minCountArray = usedCoins + [coin]
+                if minCountArray is None or usedCoins + 1 < minCountArray:  
+                    minCountArray = usedCoins + 1
                     
         visited[amount] = minCountArray
         return minCountArray
     
+#   BOTTOM-UP WITH TABULATION
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+        
