949db4a304da9f5ec18f8ab11d76f58f869aeba3
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-07 22:19:56 +0300

    Time: 3057 ms (6.79%), Space: 32.8 MB (36.56%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,27 +1,39 @@
 class Solution:
-    def findOriginalArray(self, changed: List[int]) -> List[int]:
+    # def findOriginalArray(self, changed: List[int]) -> List[int]:
         # return [] if len is odd
         # sort 
         # change to hash-map
         # check if i*2 exists for every item
-        changed.sort()
+#         changed.sort()
         
-        if len(changed) % 2 != 0: return []
+#         if len(changed) % 2 != 0: return []
         
-        # 6 3 0 1
+#         # 6 3 0 1
         
-        hash_map = Counter(changed)
-        original_list = []
+#         hash_map = Counter(changed)
+#         original_list = []
         
-        for item in changed:
-            if hash_map[item]:
-                hash_map[item]-=1
-            else: continue
+#         for item in changed:
+#             if hash_map[item]:
+#                 hash_map[item]-=1
+#             else: continue
                 
-            if  hash_map[item*2]: 
-                original_list.append(item)
-                hash_map[item*2]-=1
+#             if  hash_map[item*2]: 
+#                 original_list.append(item)
+#                 hash_map[item*2]-=1
 
-        if len(original_list) * 2 == len(changed): return original_list
-        return []
-                
+#         if len(original_list) * 2 == len(changed): return original_list
+#         return []
+
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        res = []
+        count = Counter(changed)
+        for e in changed:
+            if not count[e]: continue
+            count[e] -= 1
+            if count.get(e*2, 0) > 0:
+                res.append(e)
+                count[e*2] -= 1
+
+        return res if len(res) == len(changed) / 2 else []
