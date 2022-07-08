921ee9ed3bfa7512c73c4cb9952b22dd6b8ca9d4
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-08 12:31:17 +0300

    Update 1561-maximum-number-of-coins-you-can-get.py

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,24 +1,9 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        # sort the array
         piles.sort()
-        
-        l = len(piles)
-        # calculate max loop
-        partition = l // 3
-        count = 0
-        
-        # example piles = [9,8,7,6,5,1,2,3,4]
-        # sorted piles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        
-        # to get the max amount for us, we give bob all the smallest items => [1,2,3]
-        
-        # we are left with [4,5,6,7,8,9]
-        # out of this, alice takes the max on every loop => [5,7,9]
-        # we are left with [4,6,8]
-        # this is the max we can get
-        
-        for i in range(partition,l,2):
-            count+=piles[i]
-        
-        return count
+        n = len(piles)
+        maxCoinNum = 0
+        for i in range(n // 3):
+            startFromBackIdx = n - 1 - (i * 2)
+            maxCoinNum += piles[startFromBackIdx - 1]
+        return maxCoinNum
