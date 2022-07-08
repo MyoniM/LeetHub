fa464b50ca59ee48ab3073899bdd6fbc2eafd727
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-08 15:19:40 +0300

    Time: 67 ms (10.08%), Space: 13.9 MB (26.32%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,30 +1,25 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        st = []#0,2
-        # example case
-        # s = "(u(love)i)"
-        lenn = len(s)
-        for i in range(lenn):
-
-            # Push the index of the current
-            # opening bracket
-            if (s[i] == '('):
-                st.append(i)
-
-            # Reverse the substring starting
-            # after the last encountered opening
-            # bracket till the current character
-            elif (s[i] == ')'):
-                # i = 7
-                temp = s[st[-1]:i + 1]
-                # s[2:8]
-                # (love)
-                s = s[:st[-1]] + temp[::-1] + s[i + 1:]
-                # s = s[:2] + evol + s[8:]
-                st.pop()
-
-        # To store the modified string
-        res = ""
-        for i in range(lenn):
-            if s[i] not in ["(",")"]: res += s[i]
-        return res
+#       (ed(et(oc
+#               (ed(etco
+#                   (edocteel
+#       stack = []
+#       for loop through elems
+#           if elem is not )
+#               add to stack
+#           else
+#               while loop backwar till (
+#                   append string
+#               remove ( and put the string in the stack and continue
+#       return the first element
+        stack = []
+        for e in s:
+            if e != ")": stack.append(e)
+            else:
+                newS = ""
+                while stack and stack[-1] != "(":
+                    el = stack.pop()
+                    newS = el + newS
+                stack.pop()
+                stack.append(newS[::-1])
+        return ''.join(stack)
