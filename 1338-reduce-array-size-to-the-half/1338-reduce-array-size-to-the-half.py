43be7ab91128c2fa72bf168ab8dcd3a431c50113
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-08 13:02:18 +0300

    Time: 894 ms (54.52%), Space: 35.4 MB (51.55%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,12 +1,23 @@
-class Solution(object):
-    def minSetSize(self, arr):
-        count = Counter(arr)
-        mc = count.most_common()
-        s = 0
-        removedCount = 0
-        for i in mc:
-            if removedCount >= len(arr) // 2:
-                return s
-            s += 1
-            removedCount += i[1]
-        return s
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        
+        # arr len == 10
+        # half == 5
+        half = len(arr)//2
+        count  = Counter(arr)
+        
+        # occurence freq with order  max -> min
+        frequency = count.most_common()
+
+        val = set_count = current_frequent_elem = 0
+        
+        while val < half:
+            # get the item with max count
+            max_item_count = frequency[current_frequent_elem][1]
+            # add the count of the item to val
+            val += max_item_count
+            # increase our set count
+            set_count+=1
+            current_frequent_elem+=1
+            
+        return set_count
