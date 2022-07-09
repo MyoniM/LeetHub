cb10cb7e2bcdd2c0dbc43c58bbb59f7350a5e2ef
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-09 09:52:23 +0300

    Time: 85 ms (77.05%), Space: 14.1 MB (91.63%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -1,14 +1,19 @@
 class Solution:
-    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-
+    def validateStackSequences(self, pu: List[int], po: List[int]) -> bool:
+#         loop through pushed 
+#         if s[-1] is popped[0]
+#            yes: pop from s and popped while not same
+#            no: add to s
+#         return s is empty
         s = []
-        j = 0
-        
-        for i in pushed:
-            s.append(i)
-            
-            while s and s[-1] == popped[j]:
-                s.pop()
-                j+=1
-        if not s: return True
-        return False
+        for e in pu:
+            if s and s[-1] == po[0]:
+                while s and s[-1] == po[0]:
+                    s.pop()
+                    po.pop(0)
+                s.append(e)
+            else: s.append(e)
+        while s and s[-1] == po[0]:
+            s.pop()
+            po.pop(0)
+        return not s
