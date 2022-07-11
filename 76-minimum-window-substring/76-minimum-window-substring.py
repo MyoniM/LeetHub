831f960ce989bfdf78db89c691971adc3622b24b
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-11 16:35:00 +0300

    Time: 233 ms (29.42%), Space: 14.7 MB (82.84%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -4,26 +4,22 @@ class Solution:
         if len(t) > len(s): return ''
         
         ct = Counter(t)
+        req = len(ct)
+        found = 0
         cs = {}
         l = r = 0
         ml = [None, None]
-        
         while r < len(s):
             cs[s[r]] = cs.get(s[r], 0) + 1
-            r += 1
             
-            if len(cs) < len(ct): continue
-            while self.sHasT(cs, ct):
-                if ml[0] is None or (r - l < ml[1] - ml[0]):
-                    ml[0], ml[1] = l, r
+            if s[r] in ct and cs[s[r]] == ct[s[r]]: found += 1 
+                
+            while l <= r and found == req:
+                if ml[0] is None or (r - l + 1 < ml[1] - ml[0]):
+                    ml[0], ml[1] = l, r + 1
                 cs[s[l]] -= 1
+                if s[l] in ct and cs[s[l]] < ct[s[l]]: found -= 1
                 l += 1
-        
+            r += 1
+
         return "" if ml[0] is None else s[ml[0]: ml[1]]
-        
-        
-    def sHasT(self, cs,ct):
-        for k in ct:
-            if k not in cs or cs[k] < ct[k]:
-                return False
-        return True
