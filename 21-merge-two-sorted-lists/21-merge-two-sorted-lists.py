e335f13677acdf2cc73091252b363bfa71c8e61a
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-11 19:27:41 +0300

    Time: 52 ms (63.06%), Space: 14 MB (32.31%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -8,23 +8,23 @@ class Solution:
         if list1 is None: return list2
         if list2 is None: return list1
 
-        p1 = list1
-        p1Prev = None
-        p2 = list2
+        c1 = list1
+        c2 = list2
+        d = c = ListNode(0)
         
-        while p1 is not None and p2 is not None:
-
-            if p1.val < p2.val:
-                p1Prev = p1
-                p1 = p1.next
-            else:
-                if p1Prev is not None:
-                    p1Prev.next = p2
-                p1Prev = p2 # 1
-                p2 = p2.next
-                p1Prev.next = p1
-        
-        if p1 is None:
-            p1Prev.next = p2 
-        
-        return list1 if list1.val < list2.val else list2
+        while c1 or c2:
+            if c1 is None: 
+                c.next = c2
+                break
+            if c2 is None: 
+                c.next = c1
+                break
+                
+            if c1.val <= c2.val:
+                c.next = c1
+                c1 = c1.next
+            else:                
+                c.next = c2
+                c2 = c2.next
+            c = c.next
+        return d.next
