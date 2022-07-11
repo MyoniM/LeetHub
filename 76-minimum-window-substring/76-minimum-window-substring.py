97dd8ba3b2d733b5f39c26e003706b1a176106ce
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-11 16:24:36 +0300

    Time: 817 ms (5.86%), Space: 14.6 MB (82.84%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,13 +1,10 @@
+# O(n * m) time | O(n + m) space
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # count chars of T
-        # search for a window in S that has all counts of T
-        
         if len(t) > len(s): return ''
         
         ct = Counter(t)
         cs = {}
-        
         l = r = 0
         ml = [None, None]
         
@@ -15,6 +12,7 @@ class Solution:
             cs[s[r]] = cs.get(s[r], 0) + 1
             r += 1
             
+            if len(cs) < len(ct): continue
             while self.sHasT(cs, ct):
                 if ml[0] is None or (r - l < ml[1] - ml[0]):
                     ml[0], ml[1] = l, r
