05a2a7150e88a1824e9d36d1347c2de1ee49232e
Yonatan <61096394+MyoniM@users.noreply.github.com>
2022-07-13 17:12:38 +0300

    Time: 54 ms (52.54%), Space: 14.2 MB (39.71%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,15 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        self.helper(n, n, "", res)
+        self.helper(n, n, [], res)
         return res
         
-    def helper(self, l, r, s, res):
+    def helper(self, l, r, b, res):
         if l + r == 0:
-            res.append(s)
+            res.append("".join(b))
             return
-        if l > 0: self.helper(l - 1, r, s + "(", res)
-        if l < r: self.helper(l, r - 1, s + ")", res)
+        if l > 0: self.helper(l - 1, r, b + ["("], res)
+        if l < r: self.helper(l, r - 1, b + [")"], res)
 
             
         
