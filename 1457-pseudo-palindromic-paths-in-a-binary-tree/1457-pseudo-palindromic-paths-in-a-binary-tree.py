3912129e2f76128b02cc63469339e3cd39fd2882
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-08-23 16:45:20 +0300

    Time: 1010 ms (85.42%), Space: 104.1 MB (8.33%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    palindromicPathCount = 0
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        self.dfsHelper(root, {})
+        return self.palindromicPathCount
+    
+    def dfsHelper(self, root, counter):
+        if not root: return
+        counter[root.val] = 1 + counter.get(root.val, 0)
+        if root.left is None and root.right is None:
+            if self.isPalindrome(counter):
+                self.palindromicPathCount += 1
+            return
+        self.dfsHelper(root.left, dict(counter))
+        self.dfsHelper(root.right, dict(counter))
+
+    def isPalindrome(self, counter):
+        oddsCount = 0
+        for value in counter.values():
+            if oddsCount > 1: return False
+            if value % 2 != 0: oddsCount += 1
+        return True if oddsCount < 2 else False
