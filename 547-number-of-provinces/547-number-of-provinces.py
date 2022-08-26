82de6b2dc1655981b97527df065bde94e7cfc168
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-08-26 16:10:33 +0300

    Time: 281 ms (62.65%), Space: 14.4 MB (53.95%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,17 +1,39 @@
+class DS:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+    
+    def find(self, node):
+        if node not in self.parent:
+            self.parent[node] = node
+            self.rank[node] = 0
+        if self.parent[node] == node:
+            return node
+        return self.find(self.parent[node])
+    
+    def union(self, node1, node2):
+        find1, find2 = self.find(node1), self.find(node2)
+        if find1 == find2: return False
+        if self.rank[find1] > self.rank[find2]:
+            self.parent[find2] = find1
+            self.rank[find1] += 1
+        else:
+            self.parent[find1] = find2
+            self.rank[find2] += 1
+        return True
+    
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        l = len(isConnected)
-        visited = set()
+        ds = DS()
+        L = len(isConnected)
+        for r in range(L):
+            for c in range(L):
+                if r == c: continue
+                if isConnected[r][c] != 1: continue
+                ds.union(r, c)
         
-        count = 0
-        for i in range(l):
-            if i not in visited:
-                self.dfs(i, isConnected, visited)
-                count += 1
-        return count
-    
-    def dfs(self, i, isConnected, visited):
-        for j, val in enumerate(isConnected[i]):
-            if val == 1 and j not in visited:
-                visited.add(j)
-                self.dfs(j, isConnected, visited)
+        parents = set()
+        for i in  range(L):
+            parents.add(ds.find(i))
+            
+        return len(parents)
