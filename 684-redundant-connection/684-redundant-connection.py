4908a2be3a999016e0722d586dc3098e23c8eaaa
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-08-26 15:52:20 +0300

    Time: 56 ms (95.35%), Space: 14.4 MB (56.35%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -2,30 +2,29 @@ class DSU:
     def __init__(self):
         self.parent = {}
         self.rank = {}
-        
-    def union(self, x, y):
-        findX, findY = self.find(x), self.find(y)
-        if findX != findY:
-            if self.rank[findX] > self.rank[findY]:
-                self.parent[findY] = findX
-                self.rank[findX] += 1
-            else:
-                self.parent[findX] = findY
-                self.rank[findY] += 1
-            
-    def find(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
-            self.rank[x] = 0
-        if self.parent[x] == x:
-            return x
-        return self.find(self.parent[x])
+    
+    def find(self, node):
+        if node not in self.parent:
+            self.parent[node] = node
+            self.rank[node] = 0
+        if self.parent[node] == node:
+            return node
+        return self.find(self.parent[node])
+    
+    def union(self, node1, node2):
+        find1, find2 = self.find(node1), self.find(node2)
+        if find1 == find2: return False
+        if self.rank[find1] > self.rank[find2]:
+            self.parent[find2] = find1
+            self.rank[find1] += 1
+        else:
+            self.parent[find1] = find2
+            self.rank[find2] += 1
+        return True
     
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         dsu = DSU()
-        for a, b in edges:
-            if dsu.find(a) == dsu.find(b):
-                return [a, b]
-            dsu.union(a, b)
-        
+        for s, d in edges:
+            if not dsu.union(s, d):
+                return [s,d]
