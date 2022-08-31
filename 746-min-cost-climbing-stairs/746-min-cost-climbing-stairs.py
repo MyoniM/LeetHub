411a68a56e57c6df19708d40270d96983f317cf0
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-08-31 18:43:27 +0300

    Time: 87 ms (59.71%), Space: 13.9 MB (73.89%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,18 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        vis = {}
-        return min(self.minClimb(cost, vis, 0), self.minClimb(cost, vis, 1))
+#         vis = {}
+#         return min(self.minClimb(cost, vis, 0), self.minClimb(cost, vis, 1))
     
-    def minClimb(self, cost, vis, step):
-        if step in vis: return vis[step]
-        if step >= len(cost): return 0
-        if step == len(cost) - 1: return cost[step]
+#     def minClimb(self, cost, vis, step):
+#         if step in vis: return vis[step]
+#         if step >= len(cost): return 0
+#         if step == len(cost) - 1: return cost[step]
         
-        minCost = float('inf')
-        for s in [1,2]:
-            minCost = min(minCost, self.minClimb(cost, vis, step + s))
+#         minCost = float('inf')
+#         for s in [1,2]:
+#             minCost = min(minCost, self.minClimb(cost, vis, step + s))
         
-        vis[step] = minCost + cost[step]
-        return vis[step]
-        
-        
+#         vis[step] = minCost + cost[step]
+#         return vis[step]
+        cost.append(0)
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2]) 
+        return min(cost[0], cost[1])
