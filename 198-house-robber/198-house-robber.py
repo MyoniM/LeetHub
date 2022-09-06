8ee22661ddb4ac20578cf6b0510aabc553a14469
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-09-06 13:26:16 +0300

    Time: 49 ms (52.39%), Space: 13.9 MB (65.40%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,9 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
-
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        b = s = 0
+        for m in nums:
+            if (m + s) > b:
+                t = s
+                s = b
+                b = m + t
+                continue
+            s = b
+        return b
