0ff1d52ef4ae47864ba3012b2ce33cac7efb9201
Yonatan Merkebu <61096394+MyoniM@users.noreply.github.com>
2022-09-07 14:35:06 +0300

    Time: 271 ms (41.02%), Space: 16.5 MB (87.68%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDistance(self, str1: str, str2: str) -> int:
+        n, m = len(str1), len(str2)
+        cache = {}
+        def dfs(i, j):
+            if (i, j) in cache: return cache[(i, j)]
+            if i>n or j>m: return float("inf")
+            if i==n and j==m: return 0
+
+            if not(i>=n or j>=m) and (str1[i] == str2[j]):
+                cache[(i, j)] = dfs(i+1, j+1)
+                return cache[(i, j)]
+            insert = 1+dfs(i, j+1)
+            delete = 1+dfs(i+1, j)
+            subs = 1+dfs(i+1, j+1)
+            cache[(i, j)] = min(insert, delete, subs)
+            return cache[(i, j)]
+        return dfs(0, 0)
